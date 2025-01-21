4f9ffd62e94dd3d0b2cf3033cd07bb9770758ae2
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-21 18:21:07 -0500

    Finish 01 and work on 02 Still working out a bug in 02 where only 1 'hey' is being returned. It's like the loop is not running.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = string;
+    for (let i = 0; i < num; i++) {
+        return result;
+    }
 };
 
 // Do not edit below this line
