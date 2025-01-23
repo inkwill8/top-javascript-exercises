9d8ee47ce80ef5bb6ec3aea3229cc4c2dfeafcc5
inkwill8 <sebastien.lacasse101@gmail.com>
2025-01-23 14:45:34 -0500

    Finish 03 activity - reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,21 @@
 const reverseString = function(string) {
-    const reverseString = string.split();
-    
-    return reverseString;
+
+    //divides the string into individual chars and puts them in an array
+    const arrayString = string.split('');
+
+    //need some way to reverse the array indices
+    const reverseArrayString = arrayString.reverse();
+
+    //this is what I miss EVERY TIME. this is how you pull a value locked
+    //in a loop/function
+    let returnValue = '';
+
+    //now need to pull each index out of the array and concatenate again
+    for (const index of reverseArrayString) {
+        returnValue += index;
+    }
+
+    return returnValue;
 };
 
 // Do not edit below this line
