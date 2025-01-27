6466b480768bd2e6b495eb216d2ec02daf56cf66
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-27 18:34:34 -0500

    Pass test 01

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -10,13 +10,19 @@ const sumAll = function(startInt, endInt) {
         i++;
     }
 
+    // define a var to store the added values
+    let sum = 0;
+
     // iterate over the range[] array and add each integer to the previous value
 
     for (const int of range) {
-        int 
+
+        // below is the shorthand way to add the values of left and right operands and assign it 
+        // back to the left. so same as saying sum = sum + int
+        sum += int;
     }
 
-    return int;
+    return sum;
 
 };
 
