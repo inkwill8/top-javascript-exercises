40efb67b355b458bd407c7e53cf2083660bf15d8
inkwill8 <sebastien.lacasse101@gmail.com>
2025-01-26 21:44:33 -0500

    Work on add function Working on adding each integer to the value preceding. Very lost

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+
+    let range = [];
+
+    // given a starting and ending integer, find all the values between and INCLUDING those integers
+
+    let i = startInt;
+    while (i <= endInt) {
+        range.push(i);
+        i++;
+    }
+
+    // iterate over the range[] array and add each integer to the previous value
+
+    for (const int of range) {
+        int 
+    }
+
+    return int;
 
 };
 
