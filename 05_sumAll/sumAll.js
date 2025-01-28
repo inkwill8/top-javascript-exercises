7f4fda5ed618044bfb668f0e1d8e12a1126547a6
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-27 23:50:31 -0500

    Pass test with large number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,36 @@
 const sumAll = function(startInt, endInt) {
 
+    // define a var to hold the range of integers
     let range = [];
 
-    // given a starting and ending integer, find all the values between and INCLUDING those integers
+    // create a condition which triggers a certain function depending on which argument is greater
+    if (startInt < endInt) {
+        smallStart();
+    } else if (startInt > endInt) {
+        bigStart();
+    }
 
+
+    // given a starting and ending integer, find all the values between and INCLUDING those integers
+    function smallStart() {
     let i = startInt;
     while (i <= endInt) {
         range.push(i);
         i++;
     }
+    }
 
+    function bigStart() {
+        let i = startInt;
+        while (i >= endInt) {
+            range.push(i);
+            i--;
+        }
+    }
     // define a var to store the added values
     let sum = 0;
 
     // iterate over the range[] array and add each integer to the previous value
-
     for (const int of range) {
 
         // below is the shorthand way to add the values of left and right operands and assign it 
