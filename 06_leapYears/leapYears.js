cdd06d852596d327c91ffb33e17447fbfb59d32e
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-28 14:21:23 -0500

    Finish 06 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // to be a leap year the input must be: divisible by 4
+    if (year % 4 == 0 &&
+        year % 100 !== 0 
+        ||
+        year % 400 == 0 && 
+        year % 4 == 0)
+    {
+        // if (year % 100 == 0 &&
+        //     year % 400 == 0) {
+            return true;
+    }   else {
+        return false;
+    }
+
+    // is it divisible by 100?
+
+    // if yes, NOT a leap year UNLESS
+
+    // divisible by 400 as well
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
