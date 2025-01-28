0aac913660613a1661e1fd051b283e68e3f6ccaf
inkwill8 <sebastien.lacasse101@gmail.com>
2025-01-28 17:19:26 -0500

    Finish all tests in 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+
+  let celsius = (fahrTemp - 32) * (5 / 9);
+  const roundCels = Math.round(celsius * 10) / 10;
+
+  return roundCels;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsTemp) {
+
+  let fahrenheit = (celsTemp * 9 / 5) + 32;
+  const roundFahr = Math.round(fahrenheit * 10) / 10;
+
+  return roundFahr;
+
 };
 
 // Do not edit below this line
