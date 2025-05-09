0743ab2cdf95df252fb32d2ad6091dc95a22a53e
inkwill8 <inkwill8@proton.me>
2025-05-09 18:34:04 -0400

    Finish first part of palindrome algorithm!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let newString = string.split('').reverse().join('');
 
+return	string == newString ? true : false;
 };
+
+// palindromes('rose');
 
 // Do not edit below this line
 module.exports = palindromes;
